5343866310314ff4b35561bfeecaf5fb69c8d1ec
wilypradana <pradanawily28@gmail.com>
2023-07-11 17:31:43 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    fontFamily: {
+      sans: ["Noto Sans", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        main: "#0A6EBD",
+        secondary: "#A1C2F1",
+        thrid: "#E7CEA6",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
